204f94f6fcb563bc6dbf7d37726016d8a1cccc8e
Lucy <xlucyyu@gmail.com>
2025-11-14 10:40:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-async function Main() {
+import { Scheduler } from "./services/scheduler";
+import { getCurrentBossLB } from "./utils/getcurrentBossLB";
+import { BossLeaderboard } from "./utils/handleLB";
 
+const scheduler = new Scheduler();
+const lbHandler = new BossLeaderboard();
+
+scheduler.startScheduler(async () => {
+    console.log("scheduler running");
+}, 30);
+
+async function main() {
+    const currentBossLB = await getCurrentBossLB();
+    
 }
 
-
-Main();
+main();
